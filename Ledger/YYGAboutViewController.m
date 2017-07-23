4c6d7a7c22cca9f62b70a5f689ad66ce1dc175bd
Yan Gerasimuk <mail@yangerasimuk.com>
2017-07-23 22:49:52 +0300

    Add new ViewController with About information.

--- Ledger/YYGAboutViewController.m
+++ Ledger/YYGAboutViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "YYGAboutViewController.h"
+#import "YGTools.h"
 
 @interface YYGAboutViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *labelApplicationName;
+
+@property (weak, nonatomic) IBOutlet UILabel *labelApplicationVersion;
+
+@property (weak, nonatomic) IBOutlet UILabel *labelCopyright;
+
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *labelsCommon;
 
 @end
 
@@ -17,6 +26,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.navigationItem.title = @"About Ledger";
+    
+    self.labelApplicationName.text = @"Ledger";
+    self.labelApplicationVersion.text = [NSString stringWithFormat:@"Version: %@", [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]];
+
+    self.labelCopyright.text = @"© Yan Gerasimuk";
+    
+    // name
+    NSDictionary *attributesName = @{NSFontAttributeName:[UIFont boldSystemFontOfSize:[YGTools defaultFontSize]+6]
+                                     };
+    NSAttributedString *attributedName = [[NSAttributedString alloc] initWithString:self.labelApplicationName.text attributes:attributesName];
+    
+    self.labelApplicationName.attributedText = attributedName;
+    
+    // other labels
+    for(UILabel *label in self.labelsCommon){
+        NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:[YGTools defaultFontSize]]
+                                     };
+        
+        NSAttributedString *attributed = [[NSAttributedString alloc] initWithString:label.text attributes:attributes];
+        
+        label.attributedText = attributed;
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
